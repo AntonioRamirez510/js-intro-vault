5be61147481bcfa7f34a62fbc0f4a0b0c051785c
Antonio Ramirez <antonioramirez9614@gmail.com>
2024-06-28 12:13:48 -0500

    finished with javascript alerts and message

---
+++ index.js
@@ -0,0 +1,8 @@
+// create 3 variables whose calculations add up to 10, 40, and 39
+const first = 5 + 5;
+const second = 80/2;
+const third = (second - 1);
+// Display the combination in an alert pop-up
+const combination = first + ' - ' + second + ' - ' + third;
+const stringForUser = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:\n"
+alert(stringForUser + ' ' + combination);
